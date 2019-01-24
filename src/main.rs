c93b5879b643b12e99fe11ca0b5747470f0da402
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-01-24 23:52:43 +0100

    refactor kafka consumer thread to another function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate serde_derive;
 
 use warp::{
     *,
-    body::FullBody
+    body::FullBody,
 };
 use std::{
     io::Read,
@@ -14,24 +14,25 @@ use std::{
     time::Instant,
     time::Duration,
     sync::Arc,
-    net::SocketAddr
+    net::SocketAddr,
 };
 use rdkafka::{
     ClientConfig,
     producer::{
         FutureProducer,
-        FutureRecord
+        FutureRecord,
     },
     consumer::{
         StreamConsumer,
         Consumer,
-        CommitMode
+        CommitMode,
     },
     config::RDKafkaLogLevel,
     message::Message,
 };
 use ::log::*;
 use tokio::timer;
+use std::thread::JoinHandle;
 
 #[derive(Serialize, Deserialize)]
 enum NotaryResponse {
@@ -90,36 +91,53 @@ fn main() -> Result<(), Box<dyn Error>> {
         .and_then(move |encoded_body: String| {
             let record = FutureRecord::to(&request_topic).payload(&encoded_body).key(&encoded_body);
             let timeout = 0;
-            producer.send(record, timeout)
+            producer.send(record, timeout).map_err(reject::custom).and_then(|res| {
+                res.map(|_| encoded_body)
+                    .map_err(|(kafka_err, _)| reject::custom(kafka_err))
+            })
+        }).and_then(|encoded_body|
+            timer::Delay::new(Instant::now() + Duration::from_secs(1)).map(|_| encoded_body)
                 .map_err(reject::custom)
-                .and_then(|res| {
-                    res.map(|_| encoded_body)
-                        .map_err(|(kafka_err, _)| reject::custom(kafka_err))
-                })
-        }).and_then(|encoded_body| {
-            // wait a sec and check if the response's ready
-            timer::Delay::new(Instant::now() + Duration::from_secs(1))
-                .map(|_| encoded_body)
-                .map_err(reject::custom)
-        }).map(get_response_for_encoded_message.clone());
+        ).map(get_response_for_encoded_message.clone());
 
     let check = path!("check" / String).map(get_response_for_encoded_message);
 
-    let consumer_thread_handle = std::thread::spawn(move || {
-        info!("Starting kafka consumer thread");
-        let consumer: StreamConsumer = ClientConfig::new()
-            .set("group.id", "ubirch-notary-endpoint")
-            .set("bootstrap.servers", &brokers)
-            .set("enable.partition.eof", "false")
-            .set("session.timeout.ms", "6000")
-            .set("enable.auto.commit", "true")
-            .set_log_level(RDKafkaLogLevel::Debug)
-            .create()
-            .expect("Could not create kafka consumer");
+    let consumer_thread_handle = start_kafka_consumer_thread(&brokers, &response_topic, &error_topic, ethereum_service_responses);
 
-        consumer.subscribe(&[&response_topic, &error_topic])
-            .expect("Could not subscribe to given topics");
+    serve(check.or(submit)).run(bind_address.parse::<SocketAddr>()?);
+    consumer_thread_handle.join().unwrap();
 
+    Ok(())
+}
+
+fn log_error<F>(f: F) where F: FnOnce() -> Result<(), Box<dyn Error>> {
+    match f() {
+        Err(e) => error!("{}", e),
+        _ => ()
+    }
+}
+
+fn start_kafka_consumer_thread(
+    brokers: &str,
+    response_topic: &str,
+    error_topic: &str,
+    ethereum_service_responses: Arc<RwLock<HashMap<String, String>>>,
+) -> JoinHandle<()> {
+    info!("Starting kafka consumer thread");
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", "ubirch-notary-endpoint")
+        .set("bootstrap.servers", &brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create()
+        .expect("Could not create kafka consumer");
+
+    consumer.subscribe(&[&response_topic, &error_topic])
+        .expect("Could not subscribe to given topics");
+
+    std::thread::spawn(move || {
         let process_responses = consumer.start().for_each(|message| {
             log_error(|| {
                 let message = message?;
@@ -136,17 +154,5 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         tokio::executor::current_thread::block_on_all(process_responses)
             .expect("Error while processing responses");
-    });
-
-    serve(check.or(submit)).run(bind_address.parse::<SocketAddr>()?);
-    consumer_thread_handle.join().unwrap();
-
-    Ok(())
-}
-
-fn log_error<F>(f: F) where F: FnOnce() -> Result<(), Box<dyn Error>> {
-    match f() {
-        Err(e) => error!("{}", e),
-        _ => ()
-    }
+    })
 }
